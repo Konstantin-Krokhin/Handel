5ea390ad69f49a13d6ff7d1ac04b42c1882bdb96
Konstantin Krokhin <Kostiantyn.Krokhin@ses.com>
2021-02-28 11:07:35 +0100

    Added ArtikleOptionen DataGrids' info with checkboxes Alle

--- HandelTSE/HandelTSE/ViewModels/ArtikelOptionen.xaml.cs
+++ HandelTSE/HandelTSE/ViewModels/ArtikelOptionen.xaml.cs
@@ -28,15 +28,15 @@ namespace HandelTSE.ViewModels//.Artikelverwaltung
         {
             public string option { get; set; }
             public string preis { get; set; }
-            public CheckBox ohne_preis { get; set; }
-            public CheckBox addieren { get; set; }
         }
         public ArtikelOptionen()
         {
             InitializeComponent();
+
+            
         }
 
-        private void CloseButton_Clicked(object sender, RoutedEventArgs e) 
+        private void CloseButton_Clicked(object sender, RoutedEventArgs e)
         {
             Content = new Artikelverwaltung();
         }
@@ -48,21 +48,24 @@ namespace HandelTSE.ViewModels//.Artikelverwaltung
             if (listOption1.Visibility == Visibility.Hidden) listOption1.Visibility = Visibility.Visible;
 
             if (Option1CB.SelectedIndex == 0) { listOption1.Visibility = Visibility.Hidden;} 
-            else if (Option1CB.SelectedIndex == 1)
+            else if (Option1CB.SelectedIndex == 1) // Farbe
             {
                 Farben();
                 Data = it;
                 listOption1.ItemsSource = Data;
-            } else if (Option1CB.SelectedIndex == 2)
+                OrderColumns(listOption1);
+            } else if (Option1CB.SelectedIndex == 2) // Textil
             {
                 Grossen();
                 Data = it;
                 listOption1.ItemsSource = Data;
-            } else if (Option1CB.SelectedIndex == 3)
+                OrderColumns(listOption1);
+            } else if (Option1CB.SelectedIndex == 3) // Schuhe
             {
                 for (int i = 35; i <= 47; i++) it.Add(new items { option = i.ToString(), preis = "0.00" });
                 Data = it;
                 listOption1.ItemsSource = Data;
+                OrderColumns(listOption1);
             }
         }
 
@@ -73,24 +76,26 @@ namespace HandelTSE.ViewModels//.Artikelverwaltung
             if (listOption2.Visibility == Visibility.Hidden) listOption2.Visibility = Visibility.Visible;
 
             if (Option2CB.SelectedIndex == 0) { listOption2.Visibility = Visibility.Hidden; }
-            else if (Option2CB.SelectedIndex == 1)
+            else if (Option2CB.SelectedIndex == 1) // Farbe
             {
                 Farben();
                 Data2 = it;
                 listOption2.ItemsSource = Data2;
             }
-            else if (Option2CB.SelectedIndex == 2)
+            else if (Option2CB.SelectedIndex == 2) // Textil
             {
                 Grossen();
                 Data2 = it;
                 listOption2.ItemsSource = Data2;
             }
-            else if (Option2CB.SelectedIndex == 3)
+            else if (Option2CB.SelectedIndex == 3) // Schuhe
             {
                 for (int i = 35; i <= 47; i++) it.Add(new items { option = i.ToString(), preis = "0.00" });
                 Data2 = it;
                 listOption2.ItemsSource = Data2;
             }
+            OrderColumns(listOption2);
+
         }
 
         public void Farben()
@@ -117,9 +122,52 @@ namespace HandelTSE.ViewModels//.Artikelverwaltung
 
         private void CustomizeHeaders(object sender, System.Windows.Controls.DataGridAutoGeneratingColumnEventArgs e)
         {
+            var grid = (DataGrid)sender;
             if (e.Column.Header.ToString() == "option") e.Column.Header = "Option";
             if (e.Column.Header.ToString() == "preis") e.Column.Header = "Preis(B)";
-            if (e.Column.Header.ToString() == "ohne_preis") e.Column.Header = "ohne Pr.";
+            foreach (var item in grid.Columns)
+            {
+                if (item.Header.ToString() == "ohne Pr.") { item.Visibility = Visibility.Visible; }
+                if (item.Header.ToString() == "addieren") { item.Visibility = Visibility.Visible; }
+            }
+        }
+
+        private void OrderColumns(DataGrid grid)
+        {
+            foreach (var item in grid.Columns)
+            {
+                if (item.Header.ToString() == "Option") { item.DisplayIndex = 0; }
+                if (item.Header.ToString() == "Preis(B)") { item.DisplayIndex = 1; }
+            }
+        }
+
+        private void CheckedBox_SelectAll(object sender, RoutedEventArgs e) { CheckAllCheckboxesInColumn(sender, e, true); }
+
+        private void CheckedBox_DeselectAll(object sender, RoutedEventArgs e)
+        {
+            CheckAllCheckboxesInColumn(sender, e, false);
+        }
+
+        private void CheckAllCheckboxesInColumn(object sender, RoutedEventArgs e, bool b)
+        {
+            int i = 1;
+            DataGrid dataGrid = null;
+            if (((CheckBox)sender).Name == "ohnePreisAlleCheckBoxLeft") { dataGrid = listOption1; }
+            else if (((CheckBox)sender).Name == "ohnePreisAlleCheckBoxRight") { dataGrid = listOption2; }
+            else if (((CheckBox)sender).Name == "addierenAlleCheckBoxLeft") { i = 0; dataGrid = listOption1; }
+            else if (((CheckBox)sender).Name == "addierenAlleCheckBoxRight") { i = 0; dataGrid = listOption2; }
+
+            foreach (CheckBox ch in Artikelverwaltung.FindVisualChildren<CheckBox>(dataGrid)) { if (i % 2 == 1) ch.IsChecked = b; i++; }
+        }
+
+        private void CheckedBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UncheckedBox(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
