7c59e345f43d3bead64cb8b9b016f0c41aef2e48
Konstantin Krokhin <Kostiantyn.Krokhin@ses.com>
2021-07-07 17:21:19 +0200

    Added the notifications with the link for installing MS Access 2010 driver for DB to work in case it's not detected on the machine. Added reading from DB other columns and fields, started writing the function for saving records from view to .accdb file.

--- HandelTSE/HandelTSE/ViewModels/Personalverwaltung.xaml.cs
+++ HandelTSE/HandelTSE/ViewModels/Personalverwaltung.xaml.cs
@@ -22,6 +22,8 @@ namespace HandelTSE.ViewModels
     /// </summary>
     public partial class Personalverwaltung : UserControl
     {
+        OleDbConnection con = new OleDbConnection();
+
         public Personalverwaltung()
         {
             InitializeComponent();
@@ -30,19 +32,68 @@ namespace HandelTSE.ViewModels
             string path = (System.IO.Path.GetDirectoryName(executable));
             AppDomain.CurrentDomain.SetData("DataDirectory", path);
 
-            LoadGrid();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["Connection"].ToString();
+
+            try
+            {
+                con.Open();
+                LoadGrid();
+            }
+            catch
+            {
+                MessageBoxResult result = MessageBox.Show("Bitte installieren Sie die Microsoft Access Database Engine 2010. Möchten Sie zur Download-Seite weitergeleitet werden?", "Confirmation", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+                    System.Diagnostics.Process.Start("https://www.microsoft.com/en-us/download/confirmation.aspx?id=13255");
+                    MessageBox.Show("Nach der Installation des Treibers laden Sie bitte das Menü Personalverwaltung oder den Computer neu, falls erforderlich. ");
+                }
+                else if (result == MessageBoxResult.No)
+                {
+                    MessageBox.Show("Sie müssen den Treiber installieren, um die Daten sehen zu können.");
+                }
+
+            }
         }
 
         private void LoadGrid()
         {
-            OleDbConnection con = new OleDbConnection();
-            con.ConnectionString = ConfigurationManager.ConnectionStrings["Connection"].ToString();
-            con.Open();
             OleDbCommand cmd = new OleDbCommand();
             cmd.CommandText = "select * from [TBL_PERSONAL]";
             cmd.Connection = con;
             OleDbDataReader rd = cmd.ExecuteReader();
             grid.ItemsSource = rd;
+        }
+
+        private void Speichern_Click(object sender, RoutedEventArgs e)
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.CommandText = "insert into [TBL_PERSONAL](Name)Values(@nm)";
+            cmd.Parameters.AddWithValue("@nm", Name.Text);
+            cmd.Connection = con;
+
+            cmd.CommandText = "insert into [TBL_PERSONAL](Login)Values(@nm)";
+            cmd.Parameters.AddWithValue("@nm", Login.Text);
+            cmd.Connection = con;
+
+            cmd.CommandText = "insert into [TBL_PERSONAL](Rabatt)Values(@nm)";
+            cmd.Parameters.AddWithValue("@nm", Rabatt.Text);
+            cmd.Connection = con;
+
+            /*cmd.CommandText = "insert into [TBL_PERSONAL](1)Values(@nm)";
+            cmd.Parameters.AddWithValue("@nm", Login.Text);
+            cmd.Connection = con;*/
+
+            int result = 0;
+            try
+            {
+                result = cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                MessageBox.Show("Bitte stellen Sie sicher, dass die Verbindung mit der Datenbank hergestellt ist und der erforderliche Treiber für Microsoft Access 2010 installiert ist.");
+            }
+            
+            if (result > 0) MessageBox.Show("Ihre Änderungen wurden erfolgreich gespeichert!");
         }
     }
 }
