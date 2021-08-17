b30302bb62555693b20b687bab30a7f5bff8076c
Konstantin Krokhin <Kostiantyn.Krokhin@ses.com>
2021-08-17 15:08:52 +0200

    Added the color pickers to Darstellung

--- HandelTSE/HandelTSE/ProgramEinstellungen.xaml.cs
+++ HandelTSE/HandelTSE/ProgramEinstellungen.xaml.cs
@@ -24,5 +24,28 @@ namespace HandelTSE
         {
             InitializeComponent();
         }
+
+        private void cp1_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            if (cp1.SelectedColor.HasValue)
+            {
+                Color C = cp1.SelectedColor.Value;
+                long colorVal = Convert.ToInt64(C.B * (Math.Pow(256, 0)) + C.G * (Math.Pow(256, 1)) + C.R * (Math.Pow(256, 2)));
+            }
+            cp1.Visibility = Visibility.Collapsed;
+        }
+
+        private void cp2_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            if (cp2.SelectedColor.HasValue)
+            {
+                Color C = cp2.SelectedColor.Value;
+                long colorVal = Convert.ToInt64(C.B * (Math.Pow(256, 0)) + C.G * (Math.Pow(256, 1)) + C.R * (Math.Pow(256, 2)));
+            }
+            cp2.Visibility = Visibility.Collapsed;
+        }
+
+        private void Color1Change(object sender, RoutedEventArgs e) { if (cp1.IsOpen == false) cp1.IsOpen = true; else cp1.IsOpen = false; }
+        private void Color2Change(object sender, RoutedEventArgs e) { if (cp2.IsOpen == false) cp2.IsOpen = true; else cp2.IsOpen = false; }
     }
 }
