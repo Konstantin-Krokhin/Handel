fce9b74786c8f2dee291a627b94917179f60ff8b
Konstantin Krokhin <Kostiantyn.Krokhin@ses.com>
2021-09-01 23:04:15 +0200

    Loading Darstellung db table data onto the form, saving into db, updating, code preventing the unwanted update when chosing from comboboxes

--- HandelTSE/HandelTSE/ProgramEinstellungen.xaml.cs
+++ HandelTSE/HandelTSE/ProgramEinstellungen.xaml.cs
@@ -33,6 +33,8 @@ namespace HandelTSE
         Terminalen data = new Terminalen();
         List<Terminalen> list = new List<Terminalen>();
         OleDbCommand cmd2;
+        bool started = true;
+        bool darstellung_loaded = false;
 
         public class Terminalen
         {
@@ -87,7 +89,7 @@ namespace HandelTSE
                 if (myReader["Seriennummer"] != DBNull.Value) data.Serien = (string)myReader["Seriennummer"]; else data.Serien = "";
                 if (myReader["Kassensoftware"] != DBNull.Value) data.Kassen = (string)myReader["Kassensoftware"]; else data.Kassen = "";
                 if (myReader["VersionDerSoftware"] != DBNull.Value) data.Version = (string)myReader["VersionDerSoftware"]; else data.Version = "";
-                if (myReader["NetzwerkDatenbank"] != DBNull.Value) data.Netzwerk = (bool)myReader["NetzwerkDatenbank"]; else data.Netzwerk = false;
+                if (myReader["NetzwerkDatenbank"] != DBNull.Value) data.Netzwerk = (bool)(Boolean)myReader["NetzwerkDatenbank"];// == "0"? false : true; else data.Netzwerk = false;
                 list.Add(new Terminalen { Id = data.Id, Terminal_ID = data.Terminal_ID, Marke = data.Serien, Modell = data.Modell, Serien = data.Serien, Kassen = data.Kassen, Version = data.Version, Netzwerk = data.Netzwerk });
             }
             Data = list;
@@ -99,6 +101,7 @@ namespace HandelTSE
         {
             if (cp1.SelectedColor.HasValue)
             {
+                started = false;
                 Color C = cp1.SelectedColor.Value;
                 long colorVal = Convert.ToInt64(C.B * (Math.Pow(256, 0)) + C.G * (Math.Pow(256, 1)) + C.R * (Math.Pow(256, 2)));
 
@@ -114,6 +117,7 @@ namespace HandelTSE
         {
             if (cp2.SelectedColor.HasValue)
             {
+                started = false;
                 Color C = cp2.SelectedColor.Value;
                 long colorVal = Convert.ToInt64(C.B * (Math.Pow(256, 0)) + C.G * (Math.Pow(256, 1)) + C.R * (Math.Pow(256, 2)));
 
@@ -128,7 +132,7 @@ namespace HandelTSE
         private void Color1Change(object sender, RoutedEventArgs e) { if (cp1.IsOpen == false) cp1.IsOpen = true; else cp1.IsOpen = false; }
         private void Color2Change(object sender, RoutedEventArgs e) { if (cp2.IsOpen == false) cp2.IsOpen = true; else cp2.IsOpen = false; }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) { }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) { if (darstellung_loaded == true) started = false; }
 
         private void CustomizeHeaders(object sender, System.Windows.Controls.DataGridAutoGeneratingColumnEventArgs e) { if (e.Column.Header.ToString() == "Terminal_ID") e.Column.Header = "Terminal-ID"; }
 
@@ -145,7 +149,7 @@ namespace HandelTSE
 
             int result = 0;
             int NetzwerkDatenbankValue = 0;
-            if (NetzwerkDatenbank.IsChecked == true) NetzwerkDatenbankValue = -1; else NetzwerkDatenbankValue = 0;
+            if (NetzwerkDatenbank.IsChecked == true) NetzwerkDatenbankValue = -1;
             OleDbCommand cmd = new OleDbCommand("insert into [TBL_ProgramEinstellungenKassennetz](Id, TerminalID, MarkeDesTerminals, Modellbezeichnung, Seriennummer, Kassensoftware, VersionDerSoftware, NetzwerkDatenbank)Values('"+ ++maxID + "','" + TerminalID.Text + "','" + MarkeDesTermin.Text + "','" + Modellbezeichnung.Text + "','" + Seriennummer.Text + "','" + KassenSoftware.Text + "','" + VersionDerSoftware.Text + "','" + NetzwerkDatenbankValue + "')", con);
             try { result = cmd.ExecuteNonQuery(); MessageBox.Show("Ihre Daten wurden erfolgreich gespeichert!"); }
             catch { MessageBox.Show("Bitte stellen Sie sicher, dass die Verbindung zur Datenbank hergestellt ist und der erforderliche Treiber für Microsoft Access 2010 installiert ist oder der Datentyp der Datenbankspalte mit den Daten im Formular übereinstimmt."); }
@@ -187,6 +191,7 @@ namespace HandelTSE
             Seriennummer.Text = ((Terminalen)row.Item).Serien;
             KassenSoftware.Text = ((Terminalen)row.Item).Kassen;
             VersionDerSoftware.Text = ((Terminalen)row.Item).Version;
+            NetzwerkDatenbank.IsChecked = ((Terminalen)row.Item).Netzwerk;
         }
 
         private void HideColumns() { if (TerminalenDataGrid.Items.Count > 0 && TerminalenDataGrid.Columns.Count > 0) for (int i = 0; i < TerminalenDataGrid.Columns.Count; i++) if (i != 1 && i != 2) TerminalenDataGrid.Columns[i].Visibility = Visibility.Collapsed; }
@@ -227,6 +232,65 @@ namespace HandelTSE
         {
             if (EinstellungenTabs.SelectedIndex == 4) speichernCommonButton.IsEnabled = false;
             else if (EinstellungenTabs.IsLoaded && speichernCommonButton.IsEnabled == false) speichernCommonButton.IsEnabled = true;
+            if (EinstellungenTabs.SelectedIndex == 0 && started == true)
+            {
+                OleDbCommand cmd3 = new OleDbCommand("SELECT Hintergrundfarbe, ProgrammOberflache, ProgrammGrosse, Spaltenzahl1, Zeilenzahl1, Spaltenzahl2, Zeilenzahl2, SchriftGross, MenuFunktionen, SchnellDruck FROM [TBL_ProgramEinstellungenDarstellung]", con);
+                OleDbDataReader myReader = cmd3.ExecuteReader();
+                while (myReader.Read())
+                {
+                    if (myReader["Hintergrundfarbe"] != DBNull.Value) { Farbauswahl1.Style = (Style)FindResource("BlueButton"); Farbauswahl1.Background = (Brush)converter.ConvertFromString((string)myReader["Hintergrundfarbe"]); }
+                    if (myReader["ProgrammOberflache"] != DBNull.Value) { Farbauswahl2.Style = (Style)FindResource("BlueButton"); Farbauswahl2.Background = (Brush)converter.ConvertFromString((string)myReader["ProgrammOberflache"]); }
+                    if (myReader["ProgrammGrosse"] != DBNull.Value) ProgramGrosseBox.Text = (string)myReader["ProgrammGrosse"];
+                    if (myReader["Spaltenzahl1"] != DBNull.Value) Spaltenzahl1.Text = (string)myReader["Spaltenzahl1"];
+                    if (myReader["Zeilenzahl1"] != DBNull.Value) Zeilenzahl1.Text = (string)myReader["Zeilenzahl1"];
+                    if (myReader["Spaltenzahl2"] != DBNull.Value) Spaltenzahl2.Text = (string)myReader["Spaltenzahl2"];
+                    if (myReader["Zeilenzahl2"] != DBNull.Value) Zeilenzahl2.Text = (string)myReader["Zeilenzahl2"];
+                    if (myReader["SchriftGross"] != DBNull.Value) SchriftGrossTasten.Text = (string)myReader["SchriftGross"];
+                    if (myReader["MenuFunktionen"] != DBNull.Value) MenuFunktionenCheckbox.IsChecked = (bool)(Boolean)myReader["MenuFunktionen"];
+                    if (myReader["SchnellDruck"] != DBNull.Value) SchnellDruckBox.Text = (string)myReader["SchnellDruck"];
+                }
+                darstellung_loaded = true;
+            }
         }
+
+        private void speichernCommonButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (EinstellungenTabs.SelectedIndex == 0)
+            {
+                if (SchriftGrossTasten.Text == "" || Spaltenzahl1.Text == "" || Spaltenzahl2.Text == "" || Zeilenzahl1.Text == "" || Zeilenzahl2.Text == "") { MessageBox.Show("Korrigieren Sie Ihre Daten!"); return; }
+                OleDbCommand cmd = new OleDbCommand("CREATE TABLE [TBL_ProgramEinstellungenDarstellung] ([Id] COUNTER, [Hintergrundfarbe] TEXT(55),[ProgrammOberflache] TEXT(55),[ProgrammGrosse] TEXT(55),[Spaltenzahl1] TEXT(55),[Zeilenzahl1] TEXT(55),[Spaltenzahl2] TEXT(55),[Zeilenzahl2] TEXT(55),[SchriftGross] TEXT(55), [MenuFunktionen] YESNO, [SchnellDruck] TEXT(55))", con);
+                try { cmd.ExecuteNonQuery(); } catch { }
+
+                int result = 0, MenuFunktionenValue = 0;
+                Int32 Id = -1;
+                if (MenuFunktionenCheckbox.IsChecked == true) MenuFunktionenValue = -1;
+                OleDbCommand cmd2;
+
+                OleDbCommand IdCommand = new OleDbCommand("SELECT max(Id) from TBL_ProgramEinstellungenDarstellung", con);
+                try { Id = (Int32)IdCommand.ExecuteScalar(); } catch { }
+
+                if (Id == 0) 
+                { 
+                    cmd2 = new OleDbCommand("UPDATE [TBL_ProgramEinstellungenDarstellung] SET Hintergrundfarbe = @Hintergrundfarbe, ProgrammOberflache = @ProgrammOberflache, ProgrammGrosse = @ProgrammGrosse, Spaltenzahl1 = @Spaltenzahl1, Zeilenzahl1 = @Zeilenzahl1, Spaltenzahl2 = @Spaltenzahl2, Zeilenzahl2 = @Zeilenzahl2, SchriftGross = @SchriftGross, MenuFunktionen = @MenuFunktionen, SchnellDruck = @SchnellDruck WHERE Id = @ID", con);
+
+                    cmd2.Parameters.Add(new OleDbParameter("@Hintergrundfarbe", Farbauswahl1.Background.ToString()));
+                    cmd2.Parameters.Add(new OleDbParameter("@ProgrammOberflache", Farbauswahl2.Background.ToString()));
+                    cmd2.Parameters.Add(new OleDbParameter("@ProgrammGrosse", ProgramGrosseBox.Text));
+                    cmd2.Parameters.Add(new OleDbParameter("@Spaltenzahl1", Spaltenzahl1.Text));
+                    cmd2.Parameters.Add(new OleDbParameter("@Zeilenzahl1", Zeilenzahl1.Text));
+                    cmd2.Parameters.Add(new OleDbParameter("@Spaltenzahl2", Spaltenzahl2.Text));
+                    cmd2.Parameters.Add(new OleDbParameter("@Zeilenzahl2", Zeilenzahl2.Text));
+                    cmd2.Parameters.Add(new OleDbParameter("@SchriftGross", SchriftGrossTasten.Text));
+                    cmd2.Parameters.Add(new OleDbParameter("@MenuFunktionen", MenuFunktionenValue));
+                    cmd2.Parameters.Add(new OleDbParameter("@SchnellDruck", SchnellDruckBox.Text));
+                    cmd2.Parameters.Add(new OleDbParameter("@ID", Id));
+                }
+                else cmd2 = new OleDbCommand("insert into [TBL_ProgramEinstellungenDarstellung](Id, Hintergrundfarbe, ProgrammOberflache, ProgrammGrosse, Spaltenzahl1, Zeilenzahl1, Spaltenzahl2, Zeilenzahl2, SchriftGross, MenuFunktionen, SchnellDruck)Values('" + 0 + "','" + Farbauswahl1.Background.ToString() + "','" + Farbauswahl2.Background.ToString() + "','" + ProgramGrosseBox.Text + "','" + Spaltenzahl1.Text + "','" + Zeilenzahl1.Text + "','" + Spaltenzahl2.Text + "','" + Zeilenzahl2.Text + "','" + SchriftGrossTasten.Text + "','" + MenuFunktionenValue + "','" + SchnellDruckBox.Text + "')", con);
+                try { result = cmd2.ExecuteNonQuery(); MessageBox.Show("Ihre Daten wurden erfolgreich gespeichert!"); }
+                catch { MessageBox.Show("Bitte stellen Sie sicher, dass die Verbindung zur Datenbank hergestellt ist und der erforderliche Treiber für Microsoft Access 2010 installiert ist oder der Datentyp der Datenbankspalte mit den Daten im Formular übereinstimmt."); }
+            }
+        }
+
+        private void SchnellDruckBox_SelectionChanged(object sender, SelectionChangedEventArgs e) { if (darstellung_loaded == true) started = false; }
     }
 }
