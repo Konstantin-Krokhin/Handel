5672cba4ca2011308cab17e5c2443de1487f6629
Konstantin Krokhin <winpie1@gmail.com>
2021-11-26 14:48:10 +0100

    Finished ODBC View. Set aside the menu for later development.

---
+++ HandelTSE/HandelTSE/DatensicherungEinstellungen/Datensicherung.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using HandelTSE.DatensicherungEinstellungen;
+
+namespace HandelTSE
+{
+    /// <summary>
+    /// Interaction logic for Datensicherung.xaml
+    /// </summary>
+    public partial class Datensicherung : UserControl
+    {
+        public Datensicherung()
+        {
+            InitializeComponent();
+
+            var image = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(System.Drawing.SystemIcons.Information.Handle,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+            icon.Source = image;
+        }
+
+        private void RecordSelected(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void DatenbankDataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Offen";
+            openFileDialog.Filter = "Alle Dateien *.*|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                DatenbankDirectoryTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void SaveDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dialog.ShowDialog();
+                SaveDirectoryTextBox.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void ODBC_Clicked(object sender, RoutedEventArgs e) 
+        {
+            string caption = "Information";
+            string messageBoxText = "Wollen Sie wirklich die Daten-Einstellungen ändern?";
+            MessageBoxButton button = MessageBoxButton.OKCancel;
+            MessageBoxImage icon = MessageBoxImage.None;
+            MessageBoxResult messageResult = MessageBox.Show(messageBoxText, caption, button, icon);
+
+            // Process the user choice
+            switch (messageResult)
+            {
+                case MessageBoxResult.OK:
+                    Content = new ODBCDatensicherung();
+                    break;
+                case MessageBoxResult.Cancel:
+                    break;
+            }
+        }
+    }
+}
---
+++ HandelTSE/HandelTSE/DatensicherungEinstellungen/ODBCDatensicherung.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HandelTSE.DatensicherungEinstellungen
+{
+    /// <summary>
+    /// Interaction logic for ODBCDatensicherung.xaml
+    /// </summary>
+    public partial class ODBCDatensicherung : UserControl
+    {
+        public ODBCDatensicherung()
+        {
+            InitializeComponent();
+
+            var image = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(System.Drawing.SystemIcons.Exclamation.Handle,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+            icon.Source = image;
+        }
+
+        private void Standard_Click(object sender, RoutedEventArgs e)
+        {
+            string caption = "Information";
+            string messageBoxText = "Wollen Sie wirklich die Daten-Einstellungen ändern?";
+            MessageBoxButton button = MessageBoxButton.OKCancel;
+            MessageBoxImage icon = MessageBoxImage.None;
+            MessageBoxResult messageResult = MessageBox.Show(messageBoxText, caption, button, icon);
+
+            // Process the user choice
+            switch (messageResult)
+            {
+                case MessageBoxResult.OK:
+                    Content = new Datensicherung();
+                    break;
+                case MessageBoxResult.Cancel:
+                    break;
+            }
+        }
+    }
+}
