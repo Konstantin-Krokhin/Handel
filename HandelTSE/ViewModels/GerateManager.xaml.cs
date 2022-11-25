1164a32e4323a522f8c01b2005ddef20ba10e4f1
Konstantin-Krokhin <winpie1@gmail.com>
2022-11-25 17:21:34 +0100

    Added GerateManagement menu, in the process of building the front end part of this menu. TODO: fix dissapearance of control panel in GerateManagement view and fix allignement of the whole main content area

---
+++ HandelTSE/ViewModels/GerateManager.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HandelTSE.ViewModels
+{
+    /// <summary>
+    /// Interaction logic for GerateManager.xaml
+    /// </summary>
+    public partial class GerateManager : UserControl
+    {
+        public GerateManager()
+        {
+            InitializeComponent();
+        }
+
+        private void GerateManagerTabs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void outRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PasswordRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
