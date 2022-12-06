5df6528397a33a0bfffa1c9c3a8068fa80583efe
Konstantin-Krokhin <winpie1@gmail.com>
2022-12-06 19:05:35 +0100

    Implemented Bondrucker menu with all controls and changes of activity states using checkboxes

--- HandelTSE/HandelTSE/ViewModels/GerateManager.xaml.cs
+++ HandelTSE/HandelTSE/ViewModels/GerateManager.xaml.cs
@@ -118,5 +118,150 @@ namespace HandelTSE.ViewModels
             BegrüßungTextBox.IsEnabled = true;
             BegrussungNachBondruckTextBox.IsEnabled = true;
         }
+
+
+
+        private void BondruckerCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            BondruckerComboBox.IsEnabled = true;
+            BondruckertestenButton.IsEnabled = true;
+            weitereEinstellungenButton.IsEnabled = true;
+            GeldKassenladeGroupBox.IsEnabled = true;
+            GeldKassenladeCheckBox.IsEnabled = true;
+        }
+
+        private void BondruckerCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            BondruckerComboBox.IsEnabled = false;
+            BondruckertestenButton.IsEnabled = false;
+            weitereEinstellungenButton.IsEnabled = false;
+            GeldKassenladeGroupBox.IsEnabled = false;
+            GeldKassenladeCheckBox.IsChecked = false;
+            GeldKassenladeCheckBox.IsEnabled = false;
+        }
+
+        private void GeldKassenladeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            SteuerungRadioButton.IsEnabled = true;
+            AnderenAnschlussRadioButton.IsEnabled = true;
+            anderenAnschlusstestenButton.IsEnabled = true;
+            anderenAnschlussComboBox.IsEnabled = false;
+            SteuerungRadioButton.IsChecked = true;
+        }
+
+        private void GeldKassenladeCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SteuerungRadioButton.IsEnabled = false;
+            AnderenAnschlussRadioButton.IsEnabled = false;
+            anderenAnschlusstestenButton.IsEnabled = false;
+            anderenAnschlussComboBox.IsEnabled = false;
+        }
+
+        private void AnschlussRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            anderenAnschlussComboBox.IsEnabled = true;
+        }
+
+        private void AnschlussRadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (anderenAnschlussComboBox.IsEnabled == true) anderenAnschlussComboBox.IsEnabled = false;
+        }
+
+
+
+        private void Bondrucker2Checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+            Bondrucker2ComboBox.IsEnabled = true;
+            Bondruckertesten2Button.IsEnabled = true;
+            weitereEinstellungen2Button.IsEnabled = true;
+            GeldKassenlade2GroupBox.IsEnabled = true;
+            GeldKassenlade2CheckBox.IsEnabled = true;
+        }
+
+        private void Bondrucker2Checkbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Bondrucker2ComboBox.IsEnabled = false;
+            Bondruckertesten2Button.IsEnabled = false;
+            weitereEinstellungen2Button.IsEnabled = false;
+            GeldKassenlade2GroupBox.IsEnabled = false;
+            GeldKassenlade2CheckBox.IsEnabled = false;
+            GeldKassenlade2CheckBox.IsChecked = false;
+        }
+
+        private void GeldKassenlade2CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Steuerung2RadioButton.IsEnabled = true;
+            AnderenAnschluss2RadioButton.IsEnabled = true;
+            anderenAnschlusstesten2Button.IsEnabled = true;
+            anderenAnschluss2ComboBox.IsEnabled = false;
+            Steuerung2RadioButton.IsChecked = true;
+        }
+
+        private void GeldKassenlade2CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Steuerung2RadioButton.IsEnabled = false;
+            AnderenAnschluss2RadioButton.IsEnabled = false;
+            anderenAnschlusstesten2Button.IsEnabled = false;
+            anderenAnschluss2ComboBox.IsEnabled = false;
+        }
+
+        private void Anschluss2RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            anderenAnschluss2ComboBox.IsEnabled = true;
+        }
+
+        private void Anschluss2RadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (anderenAnschluss2ComboBox.IsEnabled == true) anderenAnschluss2ComboBox.IsEnabled = false;
+        }
+
+
+
+        private void Bondrucker3Checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+            Bondrucker3ComboBox.IsEnabled = true;
+            Bondruckertesten3Button.IsEnabled = true;
+            weitereEinstellungen3Button.IsEnabled = true;
+            GeldKassenlade3GroupBox.IsEnabled = true;
+            GeldKassenlade3CheckBox.IsEnabled = true;
+        }
+
+        private void Bondrucker3Checkbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Bondrucker3ComboBox.IsEnabled = false;
+            Bondruckertesten3Button.IsEnabled = false;
+            weitereEinstellungen3Button.IsEnabled = false;
+            GeldKassenlade3GroupBox.IsEnabled = false;
+            GeldKassenlade3CheckBox.IsEnabled = false;
+            GeldKassenlade3CheckBox.IsChecked = false;
+        }
+
+        private void GeldKassenlade3CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Steuerung3RadioButton.IsEnabled = true;
+            AnderenAnschluss3RadioButton.IsEnabled = true;
+            anderenAnschlusstesten3Button.IsEnabled = true;
+            anderenAnschluss3ComboBox.IsEnabled = false;
+            Steuerung3RadioButton.IsChecked = true;
+        }
+
+        private void GeldKassenlade3CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Steuerung3RadioButton.IsEnabled = false;
+            AnderenAnschluss3RadioButton.IsEnabled = false;
+            anderenAnschlusstesten3Button.IsEnabled = false;
+            anderenAnschluss3ComboBox.IsEnabled = false;
+        }
+
+        private void Anschluss3RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            anderenAnschluss3ComboBox.IsEnabled = true;
+        }
+
+        private void Anschluss3RadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (anderenAnschluss3ComboBox.IsEnabled == true) anderenAnschluss3ComboBox.IsEnabled = false;
+        }
+
     }
 }
