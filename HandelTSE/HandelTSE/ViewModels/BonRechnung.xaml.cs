54a6e9b54447b2d5da1aa678e4295412d41d800b
Konstantin-Krokhin <winpie1@gmail.com>
2022-12-14 21:02:01 +0100

    Added Text Alignment saving to the database for both Textboxes, and loading + Title Textbox saving and loading

--- HandelTSE/HandelTSE/ViewModels/BonRechnung.xaml.cs
+++ HandelTSE/HandelTSE/ViewModels/BonRechnung.xaml.cs
@@ -31,12 +31,12 @@ namespace HandelTSE.ViewModels
             if (con == null)
             {
                 con = MainWindow.con;
-                SQLiteCommand cmd = new SQLiteCommand("CREATE TABLE [TBL_BonRechnung] ([Id] COUNTER, [KopfzeileTextBox] TEXT(55), [BoninhaltTextBox] TEXT(55), [FusszeileTextBox] TEXT(55))", con);
+                SQLiteCommand cmd = new SQLiteCommand("CREATE TABLE [TBL_BonRechnung] ([Id] COUNTER, [TextAlignment1] TEXT(55), [TextAlignment2] TEXT(55), [TitleTextBox] TEXT(60), [KopfzeileTextBox] TEXT(555), [BoninhaltTextBox] TEXT(555), [FusszeileTextBox] TEXT(555))", con);
                 try
                 {
                     cmd.ExecuteNonQuery();
                     //Create first empty record with the proper starting ID starting from 1
-                    SQLiteCommand cmd2 = new SQLiteCommand("insert into [TBL_BonRechnung](Id, KopfzeileTextBox, BoninhaltTextBox, FusszeileTextBox)Values('" + 1 + "','" + KopfzeileTextBox.Text + "','" + BoninhaltTextBox.Text + "','" + FusszeileTextBox.Text + "')", con);
+                    SQLiteCommand cmd2 = new SQLiteCommand("insert into [TBL_BonRechnung](Id, TextAlignment1, TextAlignment2, TitleTextBox, KopfzeileTextBox, BoninhaltTextBox, FusszeileTextBox)Values('" + 1 + "','" + "C" + "','" + "C" + "','" + TitleTextBox.Text + "','" + KopfzeileTextBox.Text + "','" + BoninhaltTextBox.Text + "','" + FusszeileTextBox.Text + "')", con);
                     try { cmd2.ExecuteNonQuery(); }
                     catch { }
                 }
@@ -53,6 +53,9 @@ namespace HandelTSE.ViewModels
             SQLiteDataReader myReader = cmd2.ExecuteReader();
             while (myReader.Read())
             {
+                if (myReader["TextAlignment1"] != DBNull.Value) if ((string)myReader["TextAlignment1"] == "L") KopfzeileTextBox.HorizontalContentAlignment = HorizontalAlignment.Left; else if ((string)myReader["TextAlignment1"] == "C") KopfzeileTextBox.HorizontalContentAlignment = HorizontalAlignment.Center;
+                if (myReader["TextAlignment2"] != DBNull.Value) if ((string)myReader["TextAlignment2"] == "L") FusszeileTextBox.HorizontalContentAlignment = HorizontalAlignment.Left; else if ((string)myReader["TextAlignment2"] == "C") FusszeileTextBox.HorizontalContentAlignment = HorizontalAlignment.Center;
+                if (myReader["TitleTextBox"] != DBNull.Value) TitleTextBox.Text = (string)myReader["TitleTextBox"]; else TitleTextBox.Text = "";
                 if (myReader["KopfzeileTextBox"] != DBNull.Value) KopfzeileTextBox.Text = (string)myReader["KopfzeileTextBox"]; else KopfzeileTextBox.Text = "";
                 if (myReader["BoninhaltTextBox"] != DBNull.Value) BoninhaltTextBox.Text = (string)myReader["BoninhaltTextBox"]; else BoninhaltTextBox.Text = "";
                 if (myReader["FusszeileTextBox"] != DBNull.Value) FusszeileTextBox.Text = (string)myReader["FusszeileTextBox"]; else FusszeileTextBox.Text = "";
@@ -81,8 +84,13 @@ namespace HandelTSE.ViewModels
 
         private void SpeichernButton_Click(object sender, RoutedEventArgs e)
         {
-            SQLiteCommand cmd = new SQLiteCommand("UPDATE [TBL_BonRechnung] SET KopfzeileTextBox = @KopfzeileTextBox, BoninhaltTextBox = @BoninhaltTextBox, FusszeileTextBox = @FusszeileTextBox WHERE Id = @ID", con);
+            SQLiteCommand cmd = new SQLiteCommand("UPDATE [TBL_BonRechnung] SET TextAlignment1 = @TextAlignment1, TextAlignment2 = @TextAlignment2, TitleTextBox = @TitleTextBox, KopfzeileTextBox = @KopfzeileTextBox, BoninhaltTextBox = @BoninhaltTextBox, FusszeileTextBox = @FusszeileTextBox WHERE Id = @ID", con);
 
+            if (KopfzeileTextBox.HorizontalContentAlignment == HorizontalAlignment.Left) cmd.Parameters.Add(new SQLiteParameter("@TextAlignment1", "L"));
+            if (KopfzeileTextBox.HorizontalContentAlignment == HorizontalAlignment.Center) cmd.Parameters.Add(new SQLiteParameter("@TextAlignment1", "C"));
+            if (FusszeileTextBox.HorizontalContentAlignment == HorizontalAlignment.Left) cmd.Parameters.Add(new SQLiteParameter("@TextAlignment2", "L"));
+            if (FusszeileTextBox.HorizontalContentAlignment == HorizontalAlignment.Center) cmd.Parameters.Add(new SQLiteParameter("@TextAlignment2", "C"));
+            cmd.Parameters.Add(new SQLiteParameter("@TitleTextBox", TitleTextBox.Text));
             cmd.Parameters.Add(new SQLiteParameter("@KopfzeileTextBox", KopfzeileTextBox.Text));
             cmd.Parameters.Add(new SQLiteParameter("@BoninhaltTextBox", BoninhaltTextBox.Text));
             cmd.Parameters.Add(new SQLiteParameter("@FusszeileTextBox", FusszeileTextBox.Text));
