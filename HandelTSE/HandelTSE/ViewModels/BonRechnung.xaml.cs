ecb215691ab0697cee8841eeda36125bcc3f91d4
Konstantin-Krokhin <winpie1@gmail.com>
2022-12-14 16:16:57 +0100

    Added backend saving of the BonRechnung menu. Saves all TextBoxes and loads them accordingly when opened. TODO: save the line alignment state in TextBoxes

--- HandelTSE/HandelTSE/ViewModels/BonRechnung.xaml.cs
+++ HandelTSE/HandelTSE/ViewModels/BonRechnung.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SQLite;
 
 namespace HandelTSE.ViewModels
 {
@@ -20,9 +21,42 @@ namespace HandelTSE.ViewModels
     /// </summary>
     public partial class BonRechnung : UserControl
     {
+        public static SQLiteConnection con;
+
         public BonRechnung()
         {
             InitializeComponent();
+
+            // If the menu Stornogrunde is being open multiple times
+            if (con == null)
+            {
+                con = MainWindow.con;
+                SQLiteCommand cmd = new SQLiteCommand("CREATE TABLE [TBL_BonRechnung] ([Id] COUNTER, [KopfzeileTextBox] TEXT(55), [BoninhaltTextBox] TEXT(55), [FusszeileTextBox] TEXT(55))", con);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    //Create first empty record with the proper starting ID starting from 1
+                    SQLiteCommand cmd2 = new SQLiteCommand("insert into [TBL_BonRechnung](Id, KopfzeileTextBox, BoninhaltTextBox, FusszeileTextBox)Values('" + 1 + "','" + KopfzeileTextBox.Text + "','" + BoninhaltTextBox.Text + "','" + FusszeileTextBox.Text + "')", con);
+                    try { cmd2.ExecuteNonQuery(); }
+                    catch { }
+                }
+                catch { }
+            }
+
+            if (MainWindow.con.State != System.Data.ConnectionState.Closed) LoadData();
+
+        }
+
+        void LoadData()
+        {
+            SQLiteCommand cmd2 = new SQLiteCommand("SELECT * FROM [TBL_BonRechnung]", con);
+            SQLiteDataReader myReader = cmd2.ExecuteReader();
+            while (myReader.Read())
+            {
+                if (myReader["KopfzeileTextBox"] != DBNull.Value) KopfzeileTextBox.Text = (string)myReader["KopfzeileTextBox"]; else KopfzeileTextBox.Text = "";
+                if (myReader["BoninhaltTextBox"] != DBNull.Value) BoninhaltTextBox.Text = (string)myReader["BoninhaltTextBox"]; else BoninhaltTextBox.Text = "";
+                if (myReader["FusszeileTextBox"] != DBNull.Value) FusszeileTextBox.Text = (string)myReader["FusszeileTextBox"]; else FusszeileTextBox.Text = "";
+            }
         }
 
         private void LeftAlignment1Button_Click(object sender, RoutedEventArgs e)
@@ -43,6 +77,19 @@ namespace HandelTSE.ViewModels
         private void CenterAlignment2Button_Click(object sender, RoutedEventArgs e)
         {
             FusszeileTextBox.HorizontalContentAlignment = HorizontalAlignment.Center;
+        }
+
+        private void SpeichernButton_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteCommand cmd = new SQLiteCommand("UPDATE [TBL_BonRechnung] SET KopfzeileTextBox = @KopfzeileTextBox, BoninhaltTextBox = @BoninhaltTextBox, FusszeileTextBox = @FusszeileTextBox WHERE Id = @ID", con);
+
+            cmd.Parameters.Add(new SQLiteParameter("@KopfzeileTextBox", KopfzeileTextBox.Text));
+            cmd.Parameters.Add(new SQLiteParameter("@BoninhaltTextBox", BoninhaltTextBox.Text));
+            cmd.Parameters.Add(new SQLiteParameter("@FusszeileTextBox", FusszeileTextBox.Text));
+            cmd.Parameters.Add(new SQLiteParameter("@ID", 1));
+
+            try { int result = cmd.ExecuteNonQuery(); LoadData(); }
+            catch { MessageBox.Show("DB error!"); }
         }
     }
 }
