ef18dfb20a7c5e34f74ea0bdd31b2d3126368146
Konstantin-Krokhin <winpie1@gmail.com>
2022-12-21 14:11:35 +0100

    Added controls for managing Canvas size, changed the buttons to RepeatButton to enable multiple clicks when holding the button, the size can be also proportionally changed by typing the number and pressing enter inthe textBox, all safeguards added.

--- HandelTSE/HandelTSE/A4DokumenteRechnung.xaml.cs
+++ HandelTSE/HandelTSE/A4DokumenteRechnung.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,68 @@ using System.Windows.Shapes;
 
 namespace HandelTSE
 {
-    /// <summary>
-    /// Interaction logic for A4DokumenteRechnung.xaml
-    /// </summary>
     public partial class A4DokumenteRechnung : UserControl
     {
+
         public A4DokumenteRechnung()
         {
             InitializeComponent();
         }
+
+        private void ObenUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ObenTextBox.Text == "") ObenTextBox.Text = "0";
+            if (ObenTextBox.Text != "29.5")
+            {
+                double Top = 0;
+                Canvas.SetTop(CanvasRectangle, ++Top);
+                double Oben = double.Parse(ObenTextBox.Text, System.Globalization.CultureInfo.InvariantCulture);
+                Oben += 0.1;
+                ObenTextBox.Text = Oben.ToString();
+            }
+        }
+
+        private void ObenDownButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UntenUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (UntenTextBox.Text == "") UntenTextBox.Text = "0";
+            if (UntenTextBox.Text != "29.5")
+            {
+                CanvasRectangle.Height--;
+                double Unten = double.Parse(UntenTextBox.Text, System.Globalization.CultureInfo.InvariantCulture);
+                Unten += 0.1;
+                UntenTextBox.Text = Unten.ToString();
+            }
+        }
+
+        private void UntenDownButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (UntenTextBox.Text == "") UntenTextBox.Text = "0";
+            if (UntenTextBox.Text != "0")
+            {
+                CanvasRectangle.Height++;
+                double Unten = double.Parse(UntenTextBox.Text, System.Globalization.CultureInfo.InvariantCulture);
+                Unten -= 0.1;
+                UntenTextBox.Text = Unten.ToString();
+            }
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                if (double.Parse(UntenTextBox.Text, System.Globalization.CultureInfo.InvariantCulture) < 0 || double.Parse(UntenTextBox.Text, System.Globalization.CultureInfo.InvariantCulture) > 29.5) return;
+                if (CanvasRectangle.Height + double.Parse(UntenTextBox.Text, System.Globalization.CultureInfo.InvariantCulture) * 10 > 296) CanvasRectangle.Height -= Math.Abs(296 - (CanvasRectangle.Height + double.Parse(UntenTextBox.Text, System.Globalization.CultureInfo.InvariantCulture) * 10));
+                else CanvasRectangle.Height += Math.Abs(296 - (CanvasRectangle.Height + double.Parse(UntenTextBox.Text, System.Globalization.CultureInfo.InvariantCulture) * 10));
+            }
+        }
+
+        
     }
+
+    
 }
---
+++ HandelTSE/HandelTSE/ValidatorClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HandelTSE
+{
+    public class ValidatorClass : ValidationRule
+    {
+        public double Min { get; set; }
+
+        public double Max { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            double parameter = 0;
+
+            try
+            {
+                if (((string)value).Length > 0)
+                {
+                    parameter = Double.Parse((String)value);
+                }
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, "Illegal characters or "
+                                             + e.Message);
+            }
+
+            if ((parameter < this.Min) || (parameter > this.Max))
+            {
+                return new ValidationResult(false,
+                    "Please enter value in the range: "
+                    + this.Min + " - " + this.Max + ".");
+            }
+            return new ValidationResult(true, null);
+        }
+    }
+}
